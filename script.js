f0587db7c21ff2298e13de042c8c13555314e336
Mateusz Kucharczyk <mateusz_kucharczyk@yahoo.com>
2018-06-12 11:47:11 +0200

    first upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var a = prompt("give the base of triangle"),
+ 	h = prompt("please give a height of Triangle"),
+ 	triangleArea = a*h/2;
+ 	console.log('Triangle field with base a: ' + a + ' and height: ' + h + ' is equal to: ' + triangleArea);
